ea616ec917c0ac3f6da53885892f4d6efb92a060
adityamenon <adityamenon90@gmail.com>
2014-04-11 13:00:28 +0000

    Fixed a little bug.

--- confirmPassword.js
+++ confirmPassword.js
@@ -3,34 +3,35 @@ define(function() {
   var module = angular.module('imageUploadDirective');
 
   module.directive('confirmPassword', function () {
-    return {
-      'restrict': 'A',
-      'scope': {
-        'verifyAgainstField': '@'
-      },
-      'require': '^form',
-      'compile': function (tElement, tAttrs) {
-        return function (scope, element, attrs, formController, modelController) {
+      return {
+        'restrict': 'A',
+        'scope': {
+          'verifyAgainstField': '@'
+        },
+        'require': '^form',
+        'compile': function (tElement, tAttrs) {
+          return function (scope, element, attrs, formController, modelController) {
 
-          if (! scope.verifyAgainstField) return false;
+            if (! scope.verifyAgainstField) return false;
 
-          element.on('input', function () {
+            var fieldValidity;
 
-            var confirmPasswordValue = element.val(),
-            passwordFieldValue   = formController[scope.verifyAgainstField].$modelValue;
+            element.on('input', function () {
 
-            if (confirmPasswordValue !== passwordFieldValue) {
-              formController[attrs.name].$setValidity(attrs.name, false);
-            } else {
-              formController[attrs.name].$setValidity(attrs.name, true);
-            }
+              var confirmPasswordValue = element.val(),
+                  passwordFieldValue   = formController[scope.verifyAgainstField].$modelValue;
 
-          });
+              if (confirmPasswordValue !== passwordFieldValue) fieldValidity = false;
+              else fieldValidity = true;
+              
+              formController[attrs.name].$setValidity(attrs.name, fieldValidity);
+              scope.$apply();
 
+            });
+
+          }
         }
-      }
-    };
-  });
-
+      };
+    });
 
 });
